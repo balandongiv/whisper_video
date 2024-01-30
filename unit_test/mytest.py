1046081797f895874f75f4214e208f82d21bd157
rpb <balandongib@gmail.com>
2024-01-30 22:20:58 +0800

    Make it as class

---
+++ unit_test/mytest.py
@@ -0,0 +1,25 @@
+import unittest
+
+from whispervid.whispervid import VideoProcessor
+
+
+class TestVideoProcessor(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize VideoProcessor before each test
+        self.processor = VideoProcessor()
+
+
+    def test_process_youtube_videos(self):
+        video_list = [
+            '../unit_test/how_copyright_works_.mp4',
+            'https://youtu.be/J38Yq85ZoyY',  # Uncomment if you want to test with a YouTube URL
+        ]
+        duration = 10  # Duration in seconds, adjust as needed
+
+        # No assertion here as we are testing if it runs without errors
+        self.processor.process_youtube_videos(video_list, duration)
+
+# This allows running the tests directly from the script
+if __name__ == '__main__':
+    unittest.main()
